e5200f1a01235ca936064351b409dec4566f03ed
Jonatan Lindén <linden.jonatan@gmail.com>
2021-05-02 11:50:30 +0200

    Add support for also displaying Brotli compressed size (#47)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.js
+++ index.js
@@ -6,22 +6,45 @@ const chalk = require('chalk');
 const prettyBytes = require('pretty-bytes');
 const StreamCounter = require('stream-counter');
 const gzipSize = require('gzip-size');
+const brotliSize = require('brotli-size');
 
-module.exports = options => {
+function hasSize(sizes) {
+	return [...sizes.values()].some(size => size > 0);
+}
+
+function promisify(stream, property, event = 'end') {
+	return new Promise((resolve, reject) => {
+		stream.on(event, () => resolve(stream[property]))
+			.on('error', error => reject(error));
+	});
+}
+
+module.exports = (options = {}) => {
 	options = {
 		pretty: true,
 		showTotal: true,
+		uncompressed: options.uncompressed || !(options.gzip || options.brotli),
 		...options
 	};
 
-	let totalSize = 0;
 	let fileCount = 0;
+	const totalSize = new Map();
 
-	function log(what, size) {
+	const description = new Map([
+		['uncompressed', ''],
+		['gzip', ' (gzipped)'],
+		['brotli', ' (brotli)']
+	]);
+
+	function log(what, sizes) {
 		let {title} = options;
 		title = title ? chalk.cyan(title) + ' ' : '';
-		size = options.pretty ? prettyBytes(size) : (size + ' B');
-		fancyLog(title + what + ' ' + chalk.magenta(size) + (options.gzip ? chalk.gray(' (gzipped)') : ''));
+		const sizeStrings = [...sizes].map(([key, size]) => {
+			size = options.pretty ? prettyBytes(size) : (size + ' B');
+			return chalk.magenta(size) + chalk.gray(description.get(key));
+		});
+
+		fancyLog(title + what + ' ' + sizeStrings.join(chalk.magenta(', ')));
 	}
 
 	return through.obj((file, encoding, callback) => {
@@ -30,56 +53,67 @@ module.exports = options => {
 			return;
 		}
 
-		const finish = (error, size) => {
+		const finish = (error, sizes) => {
 			if (error) {
 				callback(new PluginError('gulp-size', error));
 				return;
 			}
 
-			totalSize += size;
+			for (const [key, size] of sizes) {
+				totalSize.set(key, size + (totalSize.get(key) || 0));
+			}
 
-			if (options.showFiles === true && size > 0) {
-				log(chalk.blue(file.relative), size);
+			if (options.showFiles === true && hasSize(sizes)) {
+				log(chalk.blue(file.relative), sizes);
 			}
 
 			fileCount++;
 			callback(null, file);
 		};
 
+		const selectedSizes = new Map();
 		if (file.isStream()) {
-			if (options.gzip) {
-				file.contents.pipe(gzipSize.stream())
-					.on('error', finish)
-					.on('end', function () {
-						finish(null, this.gzipSize);
-					});
-			} else {
-				file.contents.pipe(new StreamCounter())
-					.on('error', finish)
-					.on('finish', function () {
-						finish(null, this.bytes);
-					});
+			if (options.uncompressed) {
+				selectedSizes.set('uncompressed', promisify(file.contents.pipe(new StreamCounter()), 'bytes', 'finish'));
 			}
 
-			return;
+			if (options.gzip) {
+				selectedSizes.set('gzip', promisify(file.contents.pipe(gzipSize.stream()), 'gzipSize'));
+			}
+
+			if (options.brotli) {
+				selectedSizes.set('brotli', promisify(file.contents.pipe(brotliSize.stream()), 'brotliSize'));
+			}
 		}
 
-		if (options.gzip) {
-			(async () => {
-				try {
-					finish(null, await gzipSize(file.contents));
-				} catch (error) {
-					finish(error);
-				}
-			})();
-		} else {
-			finish(null, file.contents.length);
+		if (file.isBuffer()) {
+			if (options.uncompressed) {
+				selectedSizes.set('uncompressed', file.contents.length);
+			}
+
+			if (options.gzip) {
+				selectedSizes.set('gzip', gzipSize(file.contents));
+			}
+
+			if (options.brotli) {
+				selectedSizes.set('brotli', brotliSize.default(file.contents));
+			}
 		}
+
+		(async () => {
+			try {
+				// We want to keep the names
+				const sizes = await Promise.all([...selectedSizes].map(async ([key, size]) => [key, await size]));
+
+				finish(null, new Map(sizes));
+			} catch (error) {
+				finish(error);
+			}
+		})();
 	}, function (callback) {
-		this.size = totalSize;
-		this.prettySize = prettyBytes(totalSize);
-
-		if (!(fileCount === 1 && options.showFiles) && totalSize > 0 && fileCount > 0 && options.showTotal) {
+		this.size = totalSize.values().next().value;
+		this.prettySize = prettyBytes(this.size);
+		if (!(fileCount === 1 && options.showFiles) && hasSize(totalSize) && fileCount > 0 && options.showTotal) {
 			log(chalk.green('all files'), totalSize);
 		}
 
