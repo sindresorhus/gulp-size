2ddcbbdde8d81190c131144624c9209f30e7d171
Taras Senko <taras.senko.99@gmail.com>
2021-06-03 23:03:20 +0300

    fix after review
    Add default value for size if totalSize undefined

--- index.js
+++ index.js
@@ -111,8 +111,8 @@ module.exports = (options = {}) => {
 			}
 		})();
 	}, function (callback) {
-		this.size = totalSize.values().next().value;
-		this.prettySize = prettyBytes(this.size || 0);
+		this.size = totalSize.values().next().value || 0;
+		this.prettySize = prettyBytes(this.size);
 
 		if (!(fileCount === 1 && options.showFiles) && hasSize(totalSize) && fileCount > 0 && options.showTotal) {
 			log(chalk.green('all files'), totalSize);
